01f9106b91efe772a9bf35f27897adea0cf50f5e
Johsch112 <160511641+Johsch112@users.noreply.github.com>
2025-03-27 11:27:24 +0100

    update

---
+++ pizzeria_prg2/View/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace pizzeria_prg2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+      
+    }
+
+}
---
+++ pizzeria_prg2/ViewModel/Pizza.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pizzeria_prg2
+{
+    public abstract class Pizza
+    {
+
+
+
+
+        public string name { get; set; }
+        public string dough { get; set; }
+        public string sauce { get; set; }
+        public List<string> toppings = new List<string>();
+       
+        public void MakePizza()
+        {
+           
+        }
+      
+
+    }
+}
